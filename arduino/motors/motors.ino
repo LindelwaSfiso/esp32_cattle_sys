d6dcbbb808b1655e58b20d2ff7b5c1591269b00b
LindelwaSfiso <sfisolindelwa@gmail.com>
2025-03-29 06:55:31 +0200

    add esp32 sample codes

--- arduino/motors/motors.ino
+++ arduino/motors/motors.ino
@@ -55,7 +55,7 @@ int pos = 0;    // variable to store the servo position
 // #endif
 
 
-int servoPin = 25;
+int servoPin = 26;
 
 void setup() {
 	// Allow allocation of all timers
@@ -64,22 +64,25 @@ void setup() {
 	ESP32PWM::allocateTimer(2);
 	ESP32PWM::allocateTimer(3);
 	myservo.setPeriodHertz(50);    // standard 50 hz servo
-	myservo.attach(servoPin, 500, 2500); // attaches the servo on pin 18 to the servo object
+  myservo.attach(servoPin, 900, 2100); // attaches the servo on pin 18 to the servo object
 	// using default min/max of 1000us and 2000us
 	// different servos may require different min/max settings
 	// for an accurate 0 to 180 sweep
+  myservo.write(0);
+  delay(1000);
 }
 
 void loop() {
 
-	for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+	for (pos = 0; pos <= 170; pos += 1) { // goes from 0 degrees to 180 degrees
 		// in steps of 1 degree
 		myservo.write(pos);    // tell servo to go to position in variable 'pos'
-		delay(5);             // waits 15ms for the servo to reach the position
+		delay(15);             // waits 15ms for the servo to reach the position
 	}
-	for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+  delay(1000);
+	for (pos = 170; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
 		myservo.write(pos);    // tell servo to go to position in variable 'pos'
-		delay(5);             // waits 15ms for the servo to reach the position
+		delay(15);             // waits 15ms for the servo to reach the position
 	}
 }
 
