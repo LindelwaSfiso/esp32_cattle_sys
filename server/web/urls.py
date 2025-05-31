82448943f5da7f10af7d679babe05b8855ac55af
LindelwaSfiso <sfisolindelwa@gmail.com>
2025-05-31 11:09:42 +0200

    updated web code

--- server/web/urls.py
+++ server/web/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from web.views import index, statistics
+from web.views import index, statistics,cow_detail
 
 app_name = "web"
 urlpatterns = [
-    path('', index, name="index"),
-    path('statistics/', statistics, name="statistics"),
+	path('', index, name="index"),
+	path('statistics/', statistics, name="statistics"),
+	path('cows/<str:rfid>/', cow_detail, name='cow_detail'),
 ]
